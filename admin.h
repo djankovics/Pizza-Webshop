a8e3acd81bc0b479f882c91ef6bc6163faf88d4b
Daniel Jankovics <d.jankovics@gmail.com>
2015-12-16 15:03:06 +0100

    admin osztály üresen

---
+++ admin.h
@@ -0,0 +1,18 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+
+class Admin
+{
+public:
+    Admin();
+    void addPizza();
+    void addSzakacs();
+    void deletePizza();
+    void deleteSzakacs();
+    void editPizza();
+    void listAllPizza();
+    void logout();
+};
+
+#endif // ADMIN_H
