2375e7c31eb05a05e657b74f06098f48646c2e5c
Daniel Jankovics <d.jankovics@gmail.com>
2015-12-16 15:16:37 +0100

    Rendelések osztály igazítása a tervhez

--- rendelesek.h
+++ rendelesek.h
@@ -1,11 +1,19 @@
 #ifndef RENDELESEK_H
 #define RENDELESEK_H
 
+#include "rendelo.h"
+#include "rendeles.h"
 
 class Rendelesek
 {
+private:
+    int akt;
+    int max;
 public:
     Rendelesek();
+    void addRendeles(Rendelo rendelo);
+    void deleteRendeles(Rendeles torlendo);
+    void listRendelesek();
 };
 
 #endif // RENDELESEK_H
