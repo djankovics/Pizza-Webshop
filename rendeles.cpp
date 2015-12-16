3e65c568c50973f9a4c7174dbb3544dd5e68ed19
Daniel Jankovics <d.jankovics@gmail.com>
2015-12-16 15:13:07 +0100

    Rendelés osztály még mindig üresen, de a tervnek megfelelően

--- rendeles.cpp
+++ rendeles.cpp
@@ -1,7 +1,18 @@
 #include "rendeles.h"
 
+int Rendeles::getId() const
+{
+    return id;
+}
+
+
+string Rendeles::getTulaj() const
+{
+    return tulajdonos;
+}
+
 Rendeles::Rendeles()
 {
-
+    
 }
 
--- rendeles.h
+++ rendeles.h
@@ -1,17 +1,23 @@
 #ifndef RENDELES_H
 #define RENDELES_H
 #include <string>
+#include "pizza.h"
 
 using namespace std;
 
 class Rendeles
 {
+private:
+    int id;
+    int sajatid;
+    string tulajdonos = "";
 public:
     Rendeles();
-private:
-    int ID;
-    int SajatID;
-    string Tulajdonos = "";
+    Rendeles(string tulaj, Pizza *p);
+    int getId() const;
+    string getTulaj() const;
+    void kiir();
+
 };
 
 #endif // RENDELES_H
