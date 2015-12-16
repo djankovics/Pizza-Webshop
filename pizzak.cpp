9b208000653de1357333dcbc7651980352cf3493
Daniel Jankovics <d.jankovics@gmail.com>
2015-12-16 13:43:20 +0100

    Pizzák osztály üresen
    egyik metódus sincs elkészítve

--- pizzak.cpp
+++ pizzak.cpp
@@ -5,3 +5,27 @@ Pizzak::Pizzak()
 
 }
 
+void Pizzak::addPizza(Pizza p)
+{
+
+}
+
+void Pizzak::deletePizza(Pizza p)
+{
+
+}
+
+void Pizzak::editPizza(Pizza p)
+{
+
+}
+
+void Pizzak::feltetHozzaad(int i, string s)
+{
+
+}
+
+void Pizzak::listAllPizza()
+{
+
+}
--- pizzak.h
+++ pizzak.h
@@ -1,11 +1,19 @@
 #ifndef PIZZAK_H
 #define PIZZAK_H
 
+#include "pizza.h"
 
 class Pizzak
 {
 public:
     Pizzak();
+    void addPizza(Pizza p);
+    void deletePizza(Pizza p);
+    void editPizza(Pizza p);
+    void feltetHozzaad(int i, string s, Pizza p);
+    void listAllPizza();
+
+
 };
 
 #endif // PIZZAK_H
